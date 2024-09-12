1c240b2673a33f1336898eca7ce27bebe85d4a86
Carla <carlasantos45@hotmail.com.br>
2024-09-12 17:00:15 -0300

    att

---
+++ test/script_test.dart
@@ -0,0 +1,32 @@
+main() {
+  late Database db;
+  setUpAll(()async  {
+    databaseFactory = databaseFactoryFfi;
+    sqfliteFfiInit();
+    db = await Conexao.abrir();
+  });
+  test('teste script create table', () async {
+    var list = await db.rawQuery('SELECT * FROM tipo_procedimento');
+    expect(list.length, 3);
+  });
+}
+
+
+class DAOProfessor implements IDAOProfessor {
+  late Database _db;
+  @override
+  Future<DTOProfessor> alterarStatus(DTOProfessor dto) {
+    // TODO: implement alterarStatus
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<DTOProfessor> salvar(DTOProfessor dto) async {
+    _db = await Conexao.abrir();
+    int id = await _db
+        .rawInsert('''INSERT INTO tipo_procedimento (tipoProcedimento)
+      VALUES (?,?,?,?)''', [dto.tipoDeProcedimento]);
+    dto.id = id;
+    return dto;
+  }
+}
